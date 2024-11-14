b82a8c64d6679f0ef7ae5528a27adf8c2ffa0256
ankittrue <155477687+ankittrue@users.noreply.github.com>
2024-11-14 13:51:41 +0530

    Emp Service dev

--- src/main/java/com/springBoot/controller/EmpController1.java
+++
@@ -1,8 +0,0 @@
-package com.springBoot.controller;
-
-public class EmpController1 {
-    public static void main(String[] args) {
-        System.out.println("hello");
-        System.out.println("good morning");
-    }
-}
---
+++ src/main/java/com/springBoot/service/EmpService1.java
@@ -0,0 +1,7 @@
+package com.springBoot.service;
+
+public class EmpService1 {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
