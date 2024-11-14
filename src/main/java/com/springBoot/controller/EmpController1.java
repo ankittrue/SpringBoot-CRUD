60b81af5ca4e57a24bb55848460f16cd1ec5f149
ankittrue <155477687+ankittrue@users.noreply.github.com>
2024-11-14 13:39:32 +0530

    first commit

--- src/main/java/com/springBoot/controller/EmpController1.java
+++ src/main/java/com/springBoot/controller/EmpController1.java
@@ -3,5 +3,6 @@ package com.springBoot.controller;
 public class EmpController1 {
     public static void main(String[] args) {
         System.out.println("hello");
+        System.out.println("good morning");
     }
 }
