c1fb78d3097dccfbc6a8e5f2537682aa324fc9da
ankittrue <155477687+ankittrue@users.noreply.github.com>
2024-11-14 21:27:01 +0530

    print ankit only

--- src/main/java/com/springBoot/service/EmpService1.java
+++ src/main/java/com/springBoot/service/EmpService1.java
@@ -2,6 +2,8 @@ package com.springBoot.service;
 
 public class EmpService1 {
     public static void main(String[] args) {
+
         System.out.println("Hello");
+        System.out.println("Ankit");
     }
 }
